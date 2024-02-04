102e16638f185c5adf43a1670ec84d84cfa97f72
Nairdwood-dev <158783567+Nairdwood-dev@users.noreply.github.com>
2024-02-04 00:01:11 +0000

    New site design

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
-<html>
-  <head>
+<!doctype html>
+<html lang="en">
+<head>
     <title>Naird DNS</title>
-  </head>
-  <body>
-    <img src="./images/nd-logo.png">
-    <p>Full site coming soon!</p>
-  </body>
+	<meta content="Part of Networkz Entertainment" property="og:description" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700;900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="style.css" rel="stylesheet">
+    <meta content="Naird DNS" property="og:title" />
+    <meta content="https://www.naird-dns.com" property="og:url" />
+    <meta content="https://www.naird-dns.com/images/nd-logo.png" property="og:image" />
+    <meta content="#5819a8" data-react-helmet="true" name="theme-color" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+<body class="tgz">
+    <div class="tgz container text-center" style="height:100%; width:100%">
+        <div class="position-absolute top-50 start-50 translate-middle text-white">
+            <img src="./images/nd-logo.png" style="width: 400px;" class="animate__animated animate__zoomIn">
+            <h1 class="page-title animate__animated animate__fadeInUp animate__delay-1s">Powering DNS for Networkz Entertainment.</h1>
+            <h3 class="fw-bold animate__animated animate__fadeInUp animate__delay-2s">Thanks for stopping by!</h3>
+        </div>
+    </div>
+</body>
 </html>
